49b674cef1dd735b5425b5e31c8175077bd1c993
plauer <plauer411@gmail.com>
2014-08-21 11:46:35 -0700

    finishes shitty mvp

--- db/migrate/20140819180821_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :first_name, :last_name, :headline, :industry, :company, :linkedin_id, :location, :picture_url  
-      
-      t.timestamps
-    end
-  end
-end
--- public/js/application.js
+++ public/js/application.js
@@ -5,12 +5,11 @@ $(document).ready(function() {
       // $(".search_template").on('toggle', '.location', searchResultsField);
   }
 
-  // var result_item = $('#results .item:first');
-  // result_item.remove(); FIX TOMORROW
   
   var scanPreviousIndustryResults = function(event) {
     event.preventDefault();
     var industryPushed = $(this).attr('value');
+    console.log(industryPushed);
 
     if ($(this).attr('class') !==  'industry active') {
       $(this).addClass('active');
@@ -19,32 +18,28 @@ $(document).ready(function() {
       $(this).removeClass('active');
     };
 
-    var previous_results = $('#results .item');
-    console.log(previous_results);
-
-    
-    // var buttons = $('.search_template button');
-    // var buttonsActive = [];
-    // for (var i = 0; i < buttons.length; i ++) {
-    //   if ( $(buttons[i]).hasClass('active')) {
-    //     buttonsActive.push(buttons[i]);
-    //   };     
-    // };
-
-    // console.log(buttonsActive);
-    
-
+    var previous_results = $('#results .' + industryPushed.split(' ').join('.'));
     var counter = 0;
-    for (var i = 0; i < previous_results.length; i ++) {
-        if (previous_results.find('p:eq(1)')[i].innerHTML.indexOf(industryPushed) != -1) {
-         previous_results[i].remove();
-         counter +=1;
-        }; 
+    if (previous_results.length > 0) {
+      $(previous_results).remove();
+      counter+=1;
     };
 
+    // console.log(previous_results);
+    
+
+    // var counter = 0;
+    // for (var i = 0; i < previous_results.length; i ++) {
+    //     if (previous_results.find('p:eq(1)')[i].innerHTML.indexOf(industryPushed) != -1) {
+    //      previous_results[i].remove();
+    //      counter +=1;
+
+    //     }; 
+    // };
 
 
-    if (counter == 0) {
+
+    if (counter === 0) {
 
       var ajaxRequest = $.ajax({
         type: 'post',
@@ -54,25 +49,30 @@ $(document).ready(function() {
 
       ajaxRequest.done( function(data) {
         for (var i = 0; i < data.connections.length; i++) {
-          $('#results').append("<div class = 'item'><img><h3></h3><p></p><p></p></div>");
-          var template = $('#results .item:last');
+          $('#results').append("<div><img><h3></h3><p></p><p></p></div>");
+
+
+          var template = $('#results div:last');
+          $(template).addClass(data.industry);
           $(template).find('img').attr("src", data.connections[i].picture_url);
           $(template).find('h3').text(data.connections[i].first_name +' ' + data.connections[i].last_name);
           $(template).find('p:first').text(data.connections[i].headline);
           $(template).find('p:eq(1)').text(data.industry);
         }
-        console.log(data.connections);
       })
-
-
-
-
-
     }
 
-    // findIndustries;
-
  };
+
+
+
+
+
+
+bindEvents();
+
+});
+
 
 
   // var retrieveIndustries = function(event) {
@@ -101,13 +101,6 @@ $(document).ready(function() {
   //     console.log(data.connections);
   //   })
   // };
-
-
-
-bindEvents();
-
-});
-
 
 
 
