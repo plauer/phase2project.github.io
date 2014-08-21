c15d0d5f53eda207378f3182bfe2d32bb4a45d6a
plauer <plauer411@gmail.com>
2014-08-21 10:16:19 -0700

    adds industry flexibility

--- public/js/application.js
+++ public/js/application.js
@@ -5,14 +5,20 @@ $(document).ready(function() {
       // $(".search_template").on('toggle', '.location', searchResultsField);
   }
 
+  // var result_item = $('#results .item:first');
+  // result_item.remove(); FIX TOMORROW
   
- 
   var scanPreviousIndustryResults = function(event) {
     event.preventDefault();
+    if ($(this).attr('class') !==  'industry active') {
+      $(this).addClass('active');
+    }
+    else {$(this).removeClass('active')};
+
     var previous_results = $('#results .item');
  
     var value_state = $(this).attr('value');
-    console.log(value_state);
+    
 
     var counter = 0;
     for (var i = 0; i < previous_results.length; i ++) {
@@ -32,16 +38,13 @@ $(document).ready(function() {
       });
 
       ajaxRequest.done( function(data) {
-        var result_item = $('#results').clone().appendTo('#results');
-
         for (var i = 0; i < data.connections.length; i++) {
-          var result_item = $('#results .item:first').clone().appendTo('#results')
-          $(result_item).find('img').attr("src", data.connections[i].picture_url);
-          $(result_item).find('h3').text(data.connections[i].first_name +' ' + data.connections[i].last_name);
-          $(result_item).find('p:first').text(data.connections[i].headline);
-          $(result_item).find('p:eq(1)').text(data.industry);
-          // $(result_item).find('p:eq(2)').text(data.connections[i].location.location_name);
-
+          $('#results').append("<div class = 'item'><img><h3></h3><p></p><p></p></div>");
+          var template = $('#results .item:last');
+          $(template).find('img').attr("src", data.connections[i].picture_url);
+          $(template).find('h3').text(data.connections[i].first_name +' ' + data.connections[i].last_name);
+          $(template).find('p:first').text(data.connections[i].headline);
+          $(template).find('p:eq(1)').text(data.industry);
         }
         console.log(data.connections);
       })
